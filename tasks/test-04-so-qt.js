b0244043aaa782b3ba5a04036ac7863c4d4b2f84
Stepan Orlov <majorsteve@mail.ru>
2019-01-21 02:26:54 +0300

    Added the test-04-so-qt task set.

--- tasks/test-04-so-qt.js
+++ tasks/test-04-so-qt.js
@@ -9,7 +9,39 @@ module.exports = tasks.Tasks.fromObject({
     items: [
         {       // 1
             text: '«Бесконечная» анимация: мячик, прыгающий вверх и вниз.',
-            scene: ['html', 'bounding-ball.html'],
+            scene: ['html', 'test-04-so-qt/bouncing-ball.html'],
+            tags: ['complexity-3', 'qt', 'painting']
+        }, {    // 2
+            text: '«Бесконечная» анимация: мячик, движущийся равномерно и прямолинейно до столкновения с краями экрана; при столкновении «отражается».',
+            scene: ['html', 'test-04-so-qt/reflecting-ball.html'],
+            tags: ['complexity-3', 'qt', 'painting']
+        }, {    // 3
+            text: '«Бесконечная» анимация: эллипс фиксированной формы и размеров вращается вокруг центра (полуоси должны быть разными!).',
+            scene: ['html', 'test-04-so-qt/rotating-ellipse.html'],
+            tags: ['complexity-3', 'qt', 'painting']
+        }, {    // 4
+            text: 'Анимация длительностью 1 секунда, начинающаяся при нажатии на кнопку (например, что-нибудь падает вниз).',
+            scene: ['html', 'test-04-so-qt/falling-down-on-btnclick.html'],
+            tags: ['complexity-3', 'qt', 'painting']
+        }, {    // 5
+            text: 'слева &mdash; поле ввода текста, справа &mdash; графическая сцена, элементы которой &mdash; строки этого текста; их можно перетаскивать. Сцена генерируется при нажатии кнопки.',
+            scene: ['html', 'test-04-so-qt/drag-text.html'],
+            tags: ['complexity-3', 'qt', 'painting']
+        }, {    // 6
+            text: 'Кнопка, при нажатии на которую показывается диалог выбора цвета. Выбранный цвет рисуется на кнопке.',
+            scene: ['html', 'test-04-so-qt/color-picker.html'],
+            tags: ['complexity-3', 'qt', 'painting']
+        }, {    // 7
+            text: 'Выпадающий список с тремя элементами: «круг», «квадрат», «треугольник». При выборе элемента из списка в расположенном ниже виде графической сцены изображается соответствующий элемент.',
+            scene: ['html', 'test-04-so-qt/list-of-shapes.html'],
+            tags: ['complexity-3', 'qt', 'painting']
+        }, {    // 8
+            text: 'Виджет, на котором изображена картинка из файла, а также круг, центр которого находится там же, где указатель мыши.',
+            scene: ['html', 'test-04-so-qt/round-cursor-on-image.html'],
+            tags: ['complexity-3', 'qt', 'painting']
+        }, {    // 9
+            text: 'Виджет, на котором изображена картинка из файла. Клик на виджете приводит к тому, что картинка плавно поворачивается на 90 градусов против часовой стрелки; её центр остаётся неподвижным и всегда находится в центре виджета.',
+            scene: ['html', 'test-04-so-qt/image-rotating-on-click.html'],
             tags: ['complexity-3', 'qt', 'painting']
         }
 ]})
